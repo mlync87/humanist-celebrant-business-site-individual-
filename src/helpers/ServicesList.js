af1cf8fcebe2e14c16a693ceb869b24defc4cd80
mlync87 <martylynch35@hotmail.com>
2023-07-10 15:41:20 +0100

    menulist

---
+++ src/helpers/ServicesList.js
@@ -0,0 +1,21 @@
+import Weddings from "../assets/menuWedding.jpg";
+import NamingCeremony from "../assets/menuNaming.jpg";
+import Memorial from "../assets/menuMemorial.jpg";
+
+export const ServicesList = [
+  {
+    name: "Wedding Ceremonies",
+    image: Weddings,
+    price: "Prices available on request",
+  },
+  {
+    name: "Naming Ceremonies",
+    image: NamingCeremony,
+    price: "Prices available on request",
+  },
+  {
+    name: "Memorial Ceremonies",
+    image: Memorial,
+    price: "Prices available on request",
+  },
+];
---
+++ src/pages/Menu.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { ServicesList } from "../helpers/ServicesList";
+
+function Menu() {
+  return (
+    <div className="menu">
+      <h1 className="servicesTitle">Our Services</h1>
+      <div className="servicesList"></div>
+    </div>
+  );
+}
+
+export default Menu;
