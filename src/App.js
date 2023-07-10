50eb27f6f84d280fc256ea65c95a652c804af468
mlync87 <martylynch35@hotmail.com>
2023-07-10 17:01:06 +0100

    frontend almost completed

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
 import Services from "./pages/Services";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
@@ -13,6 +15,8 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/menu" element={<Services />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" exact component={Contact} />
         </Routes>
         <Footer />
       </Router>
