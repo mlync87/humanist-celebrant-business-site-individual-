3fffb7cec00f191cbe3462b01a24f5ed4747775e
mlync87 <martylynch35@hotmail.com>
2023-07-10 16:11:05 +0100

    services list and image cards made

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import "./App.css";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
-import Menu from "./pages/Menu";
+import Services from "./pages/Services";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
@@ -12,7 +12,7 @@ function App() {
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/menu" element={<Menu />} />
+          <Route path="/menu" element={<Services />} />
         </Routes>
         <Footer />
       </Router>
--- src/pages/Services.js
+++ src/pages/Services.js
@@ -1,18 +1,26 @@
 import React from "react";
 import { ServicesList } from "../helpers/ServicesList";
 import ServicesItem from "../components/ServicesItem";
+import "../styles/Services.css";
 
-function Menu() {
+function Services() {
   return (
     <div className="menu">
       <h1 className="servicesTitle">Our Services</h1>
       <div className="servicesList">
         {ServicesList.map((servicesItem, key) => {
-          return <ServicesItem />;
+          return (
+            <ServicesItem
+              key={key}
+              image={servicesItem.image}
+              name={servicesItem.name}
+              price={servicesItem.price}
+            />
+          );
         })}
       </div>
     </div>
   );
 }
 
-export default Menu;
+export default Services;
